08e3b300c4684ccb1e0771020222067e168f6e8c
natalyapeixoto <natalya_peixoto@hotmail.com>
2020-12-29 18:38:59 +0000

    /demo

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const port = 3000
 
 app.get('/', (req, res) => {
 	res.send('hi there, world')
-}).listen(port)
+}).listen(iport)
+
+app.get('/demo', (req, res) => {
+	res.status(418)
+	res.set('X-Tea-Pot', 'the tea')
+	res.send('hi im a tea pot')
+})
+
